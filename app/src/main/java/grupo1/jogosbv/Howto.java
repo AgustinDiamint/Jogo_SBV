cf3d99a61739deca3ad5d8479d31af83607bb4c4
Agustina Diamint <Agustina.diamint@gmail.com>
2016-06-09 20:01:51 -0300

    Novas telas, agora funcionam!

--- app/src/main/java/grupo1/jogosbv/Howto.java
+++
@@ -1,8 +0,0 @@
-package grupo1.jogosbv;
-
-/**
- * Created by Agustina on 31/05/2016.
- */
-public class Howto {
-
-}
--- app/src/main/java/grupo1/jogosbv/MainScreen.java
+++ app/src/main/java/grupo1/jogosbv/MainScreen.java
@@ -1,6 +1,7 @@
 package grupo1.jogosbv;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.TypedValue;
@@ -8,10 +9,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class MainScreen extends AppCompatActivity {
     Button bttn1;
     Button bttn2;
     TextView text;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,13 +32,71 @@ public class MainScreen extends AppCompatActivity {
         text.setTextSize(TypedValue.COMPLEX_UNIT_SP, 32);
 
         bttn1 = (Button) findViewById(R.id.button);
-        //bttn1.setOnClickListener(startActivity(getApplicationContext(this),GameScreen.class));
+
+        bttn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (MainScreen.this, GameScreen.class);
+                startActivity(intent);
+            }
+        });
 
         bttn2 = (Button) findViewById(R.id.button2);
-        //bttn2.setOnClickListener(startActivity(new Intent(getApplicationContext(this),Howto.class)););
+        bttn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent  = new Intent(MainScreen.this, HowTo.class);
+                startActivity(intent);
+            }
+        });
 
 
-    };
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "MainScreen Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://grupo1.jogosbv/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "MainScreen Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://grupo1.jogosbv/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+
 
 
 }
